7b712a7faed7a5a55316b75ea5fadd022c90acf1
hamdymohamed162002 <73902474+hamdymohamed162002@users.noreply.github.com>
2023-04-15 16:01:25 +0200

    asd

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,6 +1,6 @@
 import { createHashRouter, Outlet } from 'react-router-dom';
 import NotFound404 from './Pages/NotFound404';
-import Home from './Pages/Home';
+import Home from './Pages/home/Home';
 import Navbar from './Components/Navbar';
 import Donation from './Pages/Donation';
 import Partners from './Pages/Partners';
